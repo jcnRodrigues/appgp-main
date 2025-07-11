825e4c52080ad21283c5ada5dc39914e87eab196
Joao Claudio Nascimento Rodrigues <joao.n.rodrigues@icloud.com>
2025-07-11 11:46:57 -0300

    Criação da pasta cadastro e about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our website! We are dedicated to providing the best service possible.</p>
+    </div>
+  );
+}
---
+++ src/app/cadastro/page.tsx
@@ -0,0 +1,8 @@
+export default function Cadastro() {
+  return (
+    <div>
+      <h1>Cadastro</h1>
+      <p>Welcome to the registration page! Please fill out the form to get started.</p>
+    </div>
+  );
+}    
